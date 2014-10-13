50188bfcd10f93c3c56c3b0cffe3e333d2b050f7
Vladimir Starkov <vstarkov@yandex-team.ru>
2014-10-13 16:25:26 +0600

    refactor(lint): simplifying all the stuff

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ var fs = require('fs');
 var Comb = require('csscomb');
 
 module.exports = function (config, verbose) {
-    var out = {
-        count: 0,
-        files: [],
-        errors: {}
-    };
+    var badFiles = [];
 
     return through.obj(function (file, enc, cb) {
         if (file.isNull()) {
@@ -29,37 +25,23 @@ module.exports = function (config, verbose) {
         }
 
         var comb = new Comb(getConfig(config, file, cb) || 'csscomb');
+        var content = file.contents.toString('utf8');
+        var filename = path.relative(file.cwd, file.path)
 
-        try {
-            var processed = comb.processString(file.contents.toString('utf8'), { filename: file.path });
-        } catch (err) {
-            var filename = path.relative(file.cwd, file.path)
-
-            out.files.push(filename);
-            out.errors[filename] = err.message;
-
-            this.push(file);
-            return cb();
+        if (content === comb.processString(content, { filename: file.path })) {
+            badFiles.push(filename);
         }
+
 
         this.push(file);
         return cb();
     }, function(cb) {
 
-        if (out.files.length > 0) {
-            var message = [
-                gutil.colors.red('\nCSScomb linting failed for these files:'),
-                out.files.map(function(filename) {
-                    return filename;
-                }).join('\n'),
-                out.files.map(function(filename) {
-                    return [
-                        '\n' + filename,
-                        out.errors[filename]
-                    ].join('\n');
-                }).join('\n'),
-                '\nTotal: ' + out.files.length + ' bad files.'
-            ].join('\n');
+        if (badFiles.length) {
+            var message = gutil.colors.red([
+                '\nCSScomb linting failed for these files:',
+                badFiles.map(function(filename) { return '• ' + filename; })
+            ].join('\n'));
 
             this.emit('error', new gutil.PluginError('gulp-csscomb-lint', message));
         }
